bbc6038f145e0ac6633ba9065611d78f71f0de1b
lev <levtrifonov@mail.ru>
2021-10-24 14:12:27 +0500

    2.2 done

--- BadNews/Startup.cs
+++ BadNews/Startup.cs
@@ -1,19 +1,11 @@
 ﻿using BadNews.ModelBuilders.News;
-using BadNews.Models.News;
 using BadNews.Repositories.News;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
 using System.Globalization;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Web;
 using Serilog;
 
 namespace BadNews
@@ -55,7 +47,7 @@ namespace BadNews
             app.UseAuthorization();
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllerRoute("default", "{controller=News}/{action=Index}");
+                endpoints.MapControllerRoute("default", "{controller=News}/{action=Index}/{id?}");
                 endpoints.MapControllerRoute("status-code", "StatusCode/{code?}", new
                 {
                     controller = "Errors",
@@ -63,10 +55,10 @@ namespace BadNews
                 });
             });
 
-            app.Map("/news/fullarticle", fullArticleApp =>
-            {
-                fullArticleApp.Run(RenderFullArticlePage);
-            });
+            // app.Map("/news/fullarticle", fullArticleApp =>
+            // {
+            //     fullArticleApp.Run(RenderFullArticlePage);
+            // });
 
             // app.MapWhen(context => context.Request.Path == "/", rootPathApp =>
             // {
@@ -78,78 +70,6 @@ namespace BadNews
 
         // Региональные настройки, которые используются при обработке запросов новостей.
         private static CultureInfo culture = CultureInfo.CreateSpecificCulture("ru-ru");
-
-        private async Task RenderIndexPage(HttpContext context)
-        {
-            // Model Builder достается из DI-контейнера
-            var newsModelBuilder = context.RequestServices.GetRequiredService<INewsModelBuilder>();
-
-            // Извлекаются входные параметры запроса
-            int.TryParse(context.Request.Query["pageIndex"], out var pageIndex);
-
-            // Строится модель страницы
-            var model = newsModelBuilder.BuildIndexModel(pageIndex, false, null);
-
-            // Строится HTML для модели
-            string pageHtml = BuildIndexPageHtml(model);
-
-            // Результат записывается в ответ
-            await context.Response.WriteAsync(pageHtml);
-        }
-
-        private static string BuildIndexPageHtml(IndexModel model)
-        {
-            var articlesBuilder = new StringBuilder();
-            var articleTemplate = File.ReadAllText("./$Content/Templates/NewsArticle.hbs");
-            foreach (var articleModel in model.PageArticles)
-            {
-                var articleHtml = articleTemplate
-                    .Replace("{{header}}", articleModel.Header)
-                    .Replace("{{date}}", articleModel.Date.ToString("d MMM yyyy", culture))
-                    .Replace("{{teaser}}", articleModel.Teaser)
-                    .Replace("{{url}}", $"/news/fullarticle/{HttpUtility.UrlEncode(articleModel.Id.ToString())}");
-                articlesBuilder.AppendLine(articleHtml);
-            }
-
-            var pageTemplate = File.ReadAllText("./$Content/Templates/Index.hbs");
-            var pageHtml = pageTemplate
-                .Replace("{{articles}}", articlesBuilder.ToString())
-                .Replace("{{newerUrl}}", !model.IsFirst
-                    ? $"/news?pageIndex={HttpUtility.UrlEncode((model.PageIndex - 1).ToString())}"
-                    : "")
-                .Replace("{{olderUrl}}", !model.IsLast
-                    ? $"/news?pageIndex={HttpUtility.UrlEncode((model.PageIndex + 1).ToString())}"
-                    : "");
-            return pageHtml;
-        }
-
-        private async Task RenderFullArticlePage(HttpContext context)
-        {
-            // Model Builder достается из DI-контейнера
-            var newsModelBuilder = context.RequestServices.GetRequiredService<INewsModelBuilder>();
-
-            // Извлекаются входные параметры запроса
-            var idString = context.Request.Path.Value.Split('/').ElementAtOrDefault(1);
-            Guid.TryParse(idString, out var id);
-
-            // Строится модель страницы
-            var model = newsModelBuilder.BuildFullArticleModel(id);
-
-            // Строится HTML для модели
-            string pageHtml = BuildFullArticlePageHtml(model);
-
-            // Результат записывается в ответ
-            await context.Response.WriteAsync(pageHtml);
-        }
-
-        private static string BuildFullArticlePageHtml(FullArticleModel model)
-        {
-            var pageTemplate = File.ReadAllText("./$Content/Templates/FullArticle.hbs");
-            var pageHtml = pageTemplate
-                .Replace("{{header}}", model.Article.Header)
-                .Replace("{{date}}", model.Article.Date.ToString("d MMM yyyy", culture))
-                .Replace("{{content}}", model.Article.ContentHtml);
-            return pageHtml;
-        }
+        
     }
 }
